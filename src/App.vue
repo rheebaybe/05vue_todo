9cf8fc4f13ceca39634c3848ea7415c1be7e10da
rheebaybe <rheebaybe@naver.com>
2021-06-22 11:10:14 +0900

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <todoheader />
+  <todoinput @addTodo="addTodo;$event" />
+  <todolist :todoItems="todoItems" />
+  <todofooter />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import todoheader from './components/todoheader.vue'
+import todoinput from './components/todoinput.vue'
+import todolist from './components/todolist.vue'
+import todofooter from './components/todofooter.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name:'app',
+  components:{
+    todoheader,
+    todoinput,
+    todolist,
+    todofooter,
+  },
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+  created(){
+    for(let i=0;i<localStorage.length;i++){
+      if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+        this.todoItems.push(localStorage.key(i));
+    }
+  },
+  methods:{
+    addTodo(todoItem){
+//todoItem=작명,input.vue의 value값을 가져온것
+      localStorage.setItem(todoItem,todoItem)
+      this.todoItems.pusth(todoItem)
     }
   }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+body{
+  font-family: 'Roboto', sans-serif;
+  background: #f6f6f8;
+  text-align: center;
+}
+*{
+  margin: 0;
+  padding:0;
+}
+a:link,a:visited{
+  text-decoration: none;
+  color:#333;
+}
+.clearfix:after{
+  content: "";
+  display: block;
+  clear:both;
+}
+li{
+  list-style: none;
+}
+#app{
+  padding:0 20px
+}
+input{
+  border-style:groove;
+  widows: 70%;
+}
+button{
+  border-style: groove;
+}
+.shadow{
+  box-shadow: 5px 10px 10px rgba(0,0,0,0.03);
 }
 </style>
